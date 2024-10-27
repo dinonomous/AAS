d0c97126881b5ab053c34389ba3c845a5192f11b
dinonomous <dineshwear@gmail.com>
2024-10-28 00:01:17 +0530

    fe

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function Home() {
   const [section, setSection] = useState("");
 
-  const handleInputChange = (event) => {
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSection(event.target.value);
   };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
