d17b606c3f3762b3ff0f1b7bb585ab86576eb29e
dinonomous <dineshwear@gmail.com>
2024-10-28 00:14:51 +0530

    optimise

--- client/src/app/section/[id]/page.tsx
+++ client/src/app/section/[id]/page.tsx
@@ -28,7 +28,7 @@ export default function SectionPage() {
   }, [section]);
 
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg w-5/6 m-auto mt-8">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
